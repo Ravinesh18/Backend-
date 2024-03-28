1d9bea6331c149afe5a79db30a5fe8b78d615df4
Ravinesh Mishra <119425262+Ravinesh18@users.noreply.github.com>
2024-03-28 11:49:21 +0530

    connect to mongo

---
+++ src/index.js
@@ -0,0 +1,59 @@
+// require('dotenv').config({path:'./env'})    Will not follow this approach as it is inconsistent
+
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv" ;
+
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const app = express();
+
+(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+        console.log("Error: ",error)
+        throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+         console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR!",error)
+        throw error
+    }
+
+})()
+
+*/
