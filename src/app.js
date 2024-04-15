de06991e1248a2f8c0de3167d998efa63e8b0794
Ravinesh Mishra <119425262+Ravinesh18@users.noreply.github.com>
2024-04-15 12:26:42 +0530

    all setup made

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import { Express } from "express";
+import  express  from "express";
 import cookieParser from "cookie-parser"
 import cors from "cors";
 
-const app = Express()
+const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN , 
@@ -10,15 +10,17 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-
 app.use(express.urlencoded({extended : true , limit : "16kb"}))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
 
-export {App}
+import userRouter from "./routes/user.routes.js"
 
+app.use('/api/v1/user' , userRouter)
+
+
+
+export {app}
 
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -30,3 +30,5 @@ const uploadOnCloudinry = async (localFilePath) => {
     }
   );
 };
+
+export {uploadOnCloudinry}
