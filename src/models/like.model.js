be3494933524c33df85346751d6386047666d05c
Ravinesh Mishra <119425262+Ravinesh18@users.noreply.github.com>
2024-05-20 22:15:14 +0530

    made all models and aggregated pipeline in users

---
+++ src/models/like.model.js
@@ -0,0 +1,29 @@
+import mongoose , {Schema} from "mongoose";
+
+const likeSchema  = new Schema(
+    {
+        tweet:{
+            type : Schema.Types.ObjectId  ,
+            ref : "Tweet"
+
+        },
+        video:{
+            type : Schema.Types.ObjectId ,
+            ref : "Video"
+
+        },
+        likedBy:{
+          type: Schema.Types.ObjectId ,
+           ref:"User"
+        },
+        comment :{
+            type :Schema.Types.ObjectId,
+            ref : "Comment"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const Like  = mongoose.model("Like" , likeSchema)
